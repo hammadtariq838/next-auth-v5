c2e1b93645fb8895efc46b83968cf46683d6b667
hammadtariq838 <24100267@lums.edu.pk>
2024-07-13 21:18:18 +0500

    feat: fully functional credentials + resend

--- auth.config.ts
+++ auth.config.ts
@@ -6,6 +6,7 @@ import { LoginSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
 
 export default {
+  secret: process.env.AUTH_SECRET,
   providers: [
     Credentials({
       async authorize(credentials) {
