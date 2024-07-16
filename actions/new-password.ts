0787bd6bbf1148b7628bbbe6d4fe3e38b519449e
hammadtariq838 <24100267@lums.edu.pk>
2024-07-16 10:55:02 +0500

    fix: build errors + pre-deployment fixes + update to bcrtypjs

--- actions/new-password.ts
+++ actions/new-password.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import * as z from 'zod';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 import { NewPasswordSchema } from '@/schemas';
 import { getPasswordResetTokenByToken } from '@/data/password-reset-token';
